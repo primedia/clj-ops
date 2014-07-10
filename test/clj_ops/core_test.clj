3270293a88fab114707085d4c10f4ddee87932cd
Brian Rowe <browe@rentpath.com>
2014-07-10 10:44:35 -0400

    [BR/EC/MC][74732722] Update tests.

--- test/clj_ops/core_test.clj
+++ test/clj_ops/core_test.clj
@@ -1,9 +1,14 @@
 (ns clj-ops.core-test
   (:require [clojure.test :refer :all]
             [clj-ops.core :refer :all]
+            [cheshire.core :as json]
             [ring.mock.request :as mock]))
 
-(def app (ops-routes {:build-time "NOW"} {"ENV_FOO" "42" "ENV_BAR" "true"}))
+(def +env+ {"ENV_FOO" "42" "ENV_BAR" "true"})
+(def +configuration+ {"some" "configuration"})
+(def app (ops-routes {:build-time "NOW"}
+                     (delay +env+)
+                     (constantly +configuration+)))
 
 (deftest test-app
   (testing "heartbeat"
@@ -15,6 +20,14 @@
     (let [response (app (mock/request :get "/ops/version"))]
       (is (= (:status response) 200))
       (is (= (:body response) "{\"build-time\":\"NOW\"}"))))
+
+  (testing "env"
+    (let [response (app (mock/request :get "/ops/env.json"))]
+      (is (= +env+ (json/parse-string (:body response))))))
+
+  (testing "config"
+    (let [response (app (mock/request :get "/ops/config.json"))]
+      (is (= +configuration+ (json/parse-string (:body response))))))
   
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
