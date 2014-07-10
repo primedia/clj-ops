9a66693817ade71ab856dc6e362eb276db609989
Brian Rowe <browe@rentpath.com>
2014-07-10 10:49:34 -0400

    [BR/EC/MC][74732722] Hiccup is not a dev dependency.

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [hiccup    "1.0.5"]
                  [compojure "1.1.8"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler clj-ops.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]
-                        [cheshire  "5.3.1"]
-                        [hiccup    "1.0.5"]]}})
+                        [cheshire  "5.3.1"]]}})
